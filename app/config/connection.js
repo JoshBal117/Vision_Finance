e8d7a1d66e33829fe44b3bdd75ea47043284d3cf
gitsunitha <sunitha.pillai@gmail.com>
2020-07-22 20:54:49 -0400

    jawsmysql err check

--- app/config/connection.js
+++ app/config/connection.js
@@ -4,15 +4,19 @@
 
 // Require mysql
 var mysql = require("mysql2");
-
+var connection;
 // Set up our connection information
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "budgetapp",
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "budgetapp",
+    });
+}
 
 // Connect to the database
 connection.connect(function(err) {
